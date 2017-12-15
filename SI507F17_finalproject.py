acec18c233b537531300323c49fe69d00df580a3
git-un <rahuldeo@umich.edu>
2017-12-14 22:14:39 -0500

    works now

--- SI507F17_finalproject.py
+++ SI507F17_finalproject.py
@@ -33,7 +33,9 @@ def create_db():
         collectionName VARCHAR(255) NOT NULL,
         price NUMERIC,
         link VARCHAR(255),
-        genre VARCHAR(128)
+        genre VARCHAR(128),
+        albumart VARCHAR(255)
+
     )""")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS artist_table (
@@ -103,8 +105,8 @@ class Album():
 
 
         cur.execute("""INSERT INTO
-            album_table(collectionId, artistId, collectionName, price, link, genre)
-            values(%(collectionId)s, %(artistId)s, %(name)s, %(price)s, %(link)s, %(genre)s)
+            album_table(collectionId, artistId, collectionName, price, link, genre, albumart)
+            values(%(collectionId)s, %(artistId)s, %(name)s, %(price)s, %(link)s, %(genre)s, %(albumart)s)
             on conflict do nothing""", details)
 
         conn.commit()
@@ -167,48 +169,45 @@ def hello_artist(name):
     cur.execute("""select * from "album_table" INNER JOIN "artist_table" ON ("album_table"."artistid" = "artist_table"."artistid") 
         where lower("artistname") like lower('%""" + name + """%') OR lower("collectionname") like lower('%""" + name + """%') """)
 
-    print('yolo')
+    itunes_data = {}
+    itunes_data['results'] = cur.fetchall()
 
-    results = cur.fetchall()
-
-    print(results)
-
-
-
-
-
-    # HINT: Trying out the flickr accessing code in another file and seeing what data you get will help debug what you need to add and send to the template!
-    # HINT 2: This is almost all the same kind of nested data investigation you've done before!
-    # https://itunes.apple.com/search?term=jack+johnson&limit=25&entity=album
-    baseurl = 'https://itunes.apple.com/search?'
-    params = {}
-    params['term'] = name
-    params['limit'] = '300'
-    params['entity'] = 'album'
-    response_obj = requests.get(baseurl, params=params)
-    itunes_data = json.loads(response_obj.text)
+    if(len(itunes_data['results']) == 0):
+        baseurl = 'https://itunes.apple.com/search?'
+        params = {}
+        params['term'] = name
+        params['limit'] = '300'
+        params['entity'] = 'album'
+        response_obj = requests.get(baseurl, params=params)
+        itunes_data = json.loads(response_obj.text)
     genreList = {}
     albums = []
 
     for i in range(len(itunes_data['results'])):
         # try:
         albums.append({
-            "name" : itunes_data['results'][i]['collectionName'],
-            "img" : itunes_data['results'][i]['artworkUrl100']
+            "name" : itunes_data['results'][i]['collectionName'] if 'collectionName' in itunes_data['results'][i].keys() else itunes_data['results'][i]['collectionname'],
+            "img" : itunes_data['results'][i]['artworkUrl100'] if 'artworkUrl100' in itunes_data['results'][i].keys() else itunes_data['results'][i]['albumart'],
+            "price" : itunes_data['results'][i]['collectionPrice'] if 'collectionPrice' in itunes_data['results'][i].keys() else itunes_data['results'][i]['price'],
+            "link" : itunes_data['results'][i]['collectionViewUrl'] if 'collectionViewUrl' in itunes_data['results'][i].keys() else itunes_data['results'][i]['link']
+
             })
 
         albumDetails = {}
+
+        print(itunes_data['results'][i])
         
-        albumDetails['name'] = itunes_data['results'][i]['collectionName']
-        albumDetails['genre'] = itunes_data['results'][i]['primaryGenreName']
-        albumDetails['link'] = itunes_data['results'][i]['collectionViewUrl']
-        albumDetails['collectionId'] = itunes_data['results'][i]['collectionId']
-        albumDetails['artistName'] = itunes_data['results'][i]['artistName']
+        albumDetails['name'] = itunes_data['results'][i]['collectionName'] if 'collectionName' in itunes_data['results'][i].keys() else itunes_data['results'][i]['collectionname']
+        albumDetails['genre'] = itunes_data['results'][i]['primaryGenreName'] if 'primaryGenreName' in itunes_data['results'][i].keys() else itunes_data['results'][i]['genre']
+        albumDetails['link'] = itunes_data['results'][i]['collectionViewUrl'] if 'collectionViewUrl' in itunes_data['results'][i].keys() else itunes_data['results'][i]['link']
+        albumDetails['collectionId'] = itunes_data['results'][i]['collectionId'] if 'collectionId' in itunes_data['results'][i].keys() else itunes_data['results'][i]['collectionid']
+        albumDetails['artistName'] = itunes_data['results'][i]['artistName'] if 'artistName' in itunes_data['results'][i].keys() else itunes_data['results'][i]['artistname']
+        albumDetails['albumart'] = itunes_data['results'][i]['artworkUrl100'] if 'artworkUrl100' in itunes_data['results'][i].keys() else itunes_data['results'][i]['albumart']
         try:
-            albumDetails['price'] = itunes_data['results'][i]['collectionPrice']
+            albumDetails['price'] = itunes_data['results'][i]['collectionPrice'] if 'collectionPrice' in itunes_data['results'][i].keys() else itunes_data['results'][i]['price']
         except:
             albumDetails['price'] = 0
-        albumDetails['artistId'] = itunes_data['results'][i]['artistId']
+        albumDetails['artistId'] = itunes_data['results'][i]['artistId'] if 'artistId' in itunes_data['results'][i].keys() else itunes_data['results'][i]['artistid']
 
         alb = Album(albumDetails)
         art = Artist(albumDetails)
